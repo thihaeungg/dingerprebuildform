1eb2f6afa65b159be6e05285ab00cec8e2fda398
thihaeungg <124340704+thihaeungg@users.noreply.github.com>
2023-02-18 10:40:17 +0630

    change

--- src/DingerServiceProvider.php
+++ src/DingerServiceProvider.php
@@ -16,7 +16,7 @@ class DingerServiceProvider extends ServiceProvider
     if ($this->app->runningInConsole()) {
 
       $this->publishes([
-        __DIR__.'/../config/config.php' => config_path('dinger.php'),
+        __DIR__.'/config/config.php' => config_path('dinger.php'),
       ], 'config');
   
     }
