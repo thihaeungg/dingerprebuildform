cee611caa8d7dbabc169e44d9348288b8b59e20f
thihaeungg <124340704+thihaeungg@users.noreply.github.com>
2023-02-19 17:54:24 +0630

    change

--- src/Dinger.php
+++ src/Dinger.php
@@ -2,12 +2,48 @@
 
 namespace Hmm\Dingerprebuildform;
 
+use phpseclib\Crypt\RSA;
 use Hmm\Dingerprebuildform\Dinger;
+use Illuminate\Validation\ValidationException;
 
 class Dinger
 {
 
-    public static function getPublicKey(){
-        return config('dinger.testing');
+    public static function prebuildFormUrl(Array $items, Array $credentials){
+        return $items;
+        // $this->checkItem($items);
+
     }
+
+    public static function formCheck(Array $array){
+
+        // $array = [
+        //     "ClientId",
+        //     "publicKey",
+        //     "items",
+        //     "customerName",
+
+        // ];
+        // $publicKey =  config('dinger.public_key');
+
+        // $rsa = new RSA();
+
+        // extract($rsa->createKey(1024));
+
+        // $rsa->loadKey($publicKey); // public key
+
+        // $rsa->setEncryptionMode(2);
+
+        // $ciphertext = $rsa->encrypt($data);
+
+        // $value = base64_encode($ciphertext);
+    }
+
+
+    // public function checkItem($items){
+        
+    //     throw ValidationException::withMessages(['items must be array']);
+
+    // }
+    
 }
